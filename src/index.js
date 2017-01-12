23aa842f81e2654b590ff19671fb0abc430c612e
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-01-12 17:41:11 +0300

    handle different geometry types in stranslation; remove Antarctica from geojson

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ const d3 = require('d3');
 const d3Geo = require('d3-geo');
 
 const padding = 40;
-const backgroundColor = '#24241d';
+const backgroundColor = '#795548';
 
 const width = document.body.offsetWidth - padding * 2,
 	height = document.body.offsetHeight - padding * 2;
 
-document.body.style.backgroundColor = 'black';
+document.body.style.backgroundColor = '#00bcd4';
 document.body.style.padding = `${padding}px`;
-
-mapunitsGeoJSON.features = mapunitsGeoJSON.features.filter(
-	f => f.properties.continent !== 'Antarctica'
-)
 
 const svg = d3.select('body').append('svg')
 	.attr('width', width)
@@ -21,16 +17,16 @@ const svg = d3.select('body').append('svg')
 
 let finalGeoJSON = mapunitsGeoJSON;
 
-finalGeoJSON.features.forEach(f => {
-	if (f.geometry.type === 'Polygon') {
-		f.geometry.coordinates[0].forEach(point => point[0] -= 11);
+finalGeoJSON.features.forEach(function (feature) {
+	if (feature.geometry.type === 'Polygon') {
+		feature.geometry.coordinates[0].forEach(function (point) { point[0] -= 11 });
 		return;
 	}
 
-	if (f.geometry.type === 'MultiPolygon') {
-		f.geometry.coordinates.forEach(polygons => {
-			polygons.forEach(polygon => {
-				polygon.forEach(point => point[0] -= 11);
+	if (feature.geometry.type === 'MultiPolygon') {
+		feature.geometry.coordinates.forEach(function (polygons) {
+			polygons.forEach(function (polygon) {
+				polygon.forEach(function (point) {point[0] -= 11 });
 			});
 		});
 		return;
@@ -49,5 +45,4 @@ svg.selectAll('path')
 	.append('path')
 	.style('fill', backgroundColor)
 	.style('stroke', 'white')
-	.attr('data-country', d => d.properties.country)
 	.attr('d', d3Geo.geoPath(projection));
